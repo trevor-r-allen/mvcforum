28bc2ac1c0c4c42885626c65f8a0bb4b8646a9b8
arnonax <arnonaxelrod@hotmail.com>
2018-01-08 23:38:32 +0200

    Started Implementing LogingAs<T>

---
+++ MVCForumAutomation/LoginPage.cs
@@ -0,0 +1,22 @@
+namespace MVCForumAutomation
+{
+    internal class LoginPage
+    {
+        public string Username
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public string Password
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public void LogOn()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
