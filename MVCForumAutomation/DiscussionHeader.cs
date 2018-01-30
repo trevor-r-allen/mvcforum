e789ca41c7d12d3e0faa3c171722706fdb4a99bf
arnonax <arnonaxelrod@hotmail.com>
2018-01-30 23:25:49 +0200

    Logger works

--- MVCForumAutomation/DiscussionHeader.cs
+++ MVCForumAutomation/DiscussionHeader.cs
@@ -8,10 +8,12 @@ namespace MVCForumAutomation
     public class DiscussionHeader
     {
         private readonly IWebElement _topicRow;
+        private readonly IWebDriver _webDriver;
 
-        public DiscussionHeader(IWebElement topicRow)
+        public DiscussionHeader(IWebElement topicRow, IWebDriver webDriver)
         {
             _topicRow = topicRow;
+            _webDriver = webDriver;
         }
 
         public string Title
@@ -30,7 +32,7 @@ namespace MVCForumAutomation
                 var link = _topicRow.FindElement(By.CssSelector("h3 a"));
                 link.Click();
 
-                var driver = ((IWrapsDriver)_topicRow).WrappedDriver;
+                var driver = _webDriver;
                 return new Discussion(driver);
             }
         }
