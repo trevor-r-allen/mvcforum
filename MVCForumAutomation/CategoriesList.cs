7dc5cf07c11b7797e536d0aeb796e0dc6e5974b1
arnonax <arnonaxelrod@hotmail.com>
2018-02-15 16:25:08 +0200

    Impelemented MVCForumClient.Categories and CategoriesList.Select

--- MVCForumAutomation/CategoriesList.cs
+++ MVCForumAutomation/CategoriesList.cs
@@ -1,10 +1,27 @@
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
 namespace MVCForumAutomation
 {
     public class CategoriesList
     {
+        private readonly IWebElement _categoriesList;
+
+        public CategoriesList(IWebDriver webDriver)
+        {
+            _categoriesList = webDriver.FindElement(By.ClassName("categories-box"));
+        }
+
         public CategoryView Select(Category category)
         {
-            throw new System.NotImplementedException();
+            var allLinks = _categoriesList.FindElements(By.TagName("a"));
+            var linkToSelect = allLinks.SingleOrDefault(link => link.Text == category.Name);
+            Assert.IsNotNull(linkToSelect, $"Category {category} was not found in categories list");
+
+            linkToSelect.Click();
+
+            return new CategoryView();
         }
     }
 }
