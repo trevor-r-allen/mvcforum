d890e37085842fc143ad57e2b216a5b040950629
arnonax <arnonaxelrod@hotmail.com>
2018-02-15 21:20:26 +0200

    Implemented Equals - test DiscussionsCanBeFilteredByCategory passes!

--- MVCForumAutomation/DiscussionIdentifier.cs
+++ MVCForumAutomation/DiscussionIdentifier.cs
@@ -8,5 +8,23 @@ namespace MVCForumAutomation
         }
 
         public string Title { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            var other = obj as DiscussionIdentifier;
+            return other != null && Equals(other);
+        }
+
+        protected bool Equals(DiscussionIdentifier other)
+        {
+            return string.Equals(Title, other.Title);
+        }
+
+        public override int GetHashCode()
+        {
+            return Title.GetHashCode();
+        }
     }
 }
