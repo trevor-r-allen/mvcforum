c740194619eb8062c86cd45b08f7aefbf32420f2
trevor-r-allen <tallen2699@gmail.com>
2021-03-30 16:59:51 -0600

    created test project and first test pseudocode

---
+++ MVCForumAutomation/SanityTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MVCForumAutomation
+{
+    [TestClass]
+    public class SanityTests
+    {
+        /*
+        Login as a registered user
+        Start a discussion titled "Hi!" and body "dummy body"
+        Enter the site as an anonymous user (from another browser)
+        Verify that a discussion titled "Hi!" appears
+        Open that discussion
+        Verify that the body of the discussion is "dummy body"
+        */
+        [TestMethod]
+        public void WhenARegisteredUserStartsADiscussionOtherAnonymousUsersCanSeeIt()
+        {
+            const string body = "dummy body";
+            LoggedInUser userA = MVCForum.RegisterNewUserAndLogin();
+            Discussion createdDiscussion = userA.CreateDiscussion(Discussion.With.Body(body));
+
+            MVCForumClient anonymousUser = new MVCForumClient();
+            DiscussionHeader latestHeader = anonymousUser.LatestDiscussion.Top;
+            Assert.AreEqual(createdDiscussion.Title, latestHeader.Title, "The title of the latest discussion should match the one we created");
+            Discussion viewedDiscussion = latestHeader.OpenDiscussion();
+            Assert.AreEqual(body, viewedDiscussion.Body, "The body of the latest discussion should match the one we created");
+        }
+    }
+}
